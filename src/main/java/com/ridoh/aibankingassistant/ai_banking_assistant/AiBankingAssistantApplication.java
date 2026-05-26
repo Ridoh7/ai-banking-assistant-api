9088bb4eb5cf1632f025615ae8a36a85bc79e951
Ridoh Lawal <ridohlawal96@gmail.com>
2026-05-26 19:44:54 +0100

    Initial Spring Boot project foundation setup

---
+++ src/main/java/com/ridoh/aibankingassistant/ai_banking_assistant/AiBankingAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.ridoh.aibankingassistant.ai_banking_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiBankingAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiBankingAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ridoh/aibankingassistant/ai_banking_assistant/AiBankingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ridoh.aibankingassistant.ai_banking_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiBankingAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
